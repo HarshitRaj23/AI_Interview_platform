5162b39a670d04d6c0ba946b25686febfcaf6c18
Harshit Raj <harshitraj23092004@gmail.com>
2025-11-16 18:20:32 +0530

    show generated interview on the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,24 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
 import { dummyInterviews } from '@/constants'
+import { getCurrentUser, getInterviewsByUserId, getLatestInteviews } from '@/lib/actions/auth.action'
 import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+ 
 
-const page = () => {
+const Page = async() => { 
+  const user = await getCurrentUser();
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInteviews({userId: user?.id!})
+  ]);
+
+  const hasPastInterviews = userInterviews?.length > 0;
+  const hasUpcomingInterviews = latestInterviews?.length > 0;
+
   return (
     <>
       <section className='card-cta'>
@@ -23,22 +35,30 @@ const page = () => {
       <section className='flex floex-col gap-6 mt-8'>
         <h2>Your Interviews</h2>
         <div className='interview-section'>
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/>
-          ))}
+          { hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id}/>
+            ))
+          ) : (
+            <p>You have no past interviews. </p>
+          )}
         </div>
       </section>
 
       <section className='flex floex-col gap-6 mt-8 mb-16'>
         <h2>Take an Interviews</h2>
         <div className='interview-section'>
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/>
-          ))}
+          { hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id}/>
+            ))
+          ) : (
+            <p>There are no new interview available.</p>
+          )}
         </div>
       </section>
     </>
   )
 }
 
-export default page
+export default Page
